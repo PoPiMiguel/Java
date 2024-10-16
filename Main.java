8df01ecf19e6c77788a9cf69ac0ad1aa7cb37dea
Miguel Porras Pizarro <151204329+PoPiMiguel@users.noreply.github.com>
2024-10-16 20:07:12 +0200

    Bucles
    Código de java introducción a los bucles

---
+++ Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce un numero");
+        System.out.println("Hasta que no introduzca el numero 0 no terminara la opercación");
+        int num = sc.nextInt();
+        while (num != 0) {
+            //Haremos el cuadrado del numero introducido
+            double potencia = Math.pow(num, 2);
+
+            //Comprobamos que el numero sea par
+            String esPar = ((num % 2) == 0) ? " es par" : "es Impar";
+
+            //Comprobamos que sea positivo o negativo
+            String esPos = (num > 0) ? "es positivo" : "es negativo";
+
+            System.out.println(" El Número " + num + " " + esPar + " " + esPos + " y su cuadrado es: " + potencia);
+
+            System.out.println("Dime otro numero");
+            Scanner rt = new Scanner(System.in);
+            num = rt.nextInt();
+        }
+    }
+}
